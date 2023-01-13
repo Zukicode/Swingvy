69a89763562a5428fb540bd2181dff83d78391e3
Zukicode <zukicode@gmail.com>
2023-01-14 00:50:09 +0200

    add header section

--- code/open-menu.js
+++ code/open-menu.js
@@ -2,8 +2,14 @@ let btn = document.querySelector('.hamburger_menu_btn');
 let close_btn = document.querySelector('.close__menu');
 
 //Function for open and close hamburger menu
-const openMenu = () => document.querySelector('.hamburger__menu').classList.add('active');
-const closeMenu = () => document.querySelector('.hamburger__menu').classList.remove('active');
+const openMenu = () => {
+   document.querySelector('.hamburger__menu').classList.add('active');
+   document.querySelector('body').style.overflow = 'hidden';
+}
+const closeMenu = () => {
+   document.querySelector('.hamburger__menu').classList.remove('active');
+   document.querySelector('body').style.overflow = 'auto';
+}
 
 btn.addEventListener('click', openMenu);
 close_btn.addEventListener('click', closeMenu);
